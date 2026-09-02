63f958503c76d4e81cd1d4640a7a60ff772f6729
Lukas Bauer <lukas_bauer@outlook.cz>
2026-09-02 20:34:57 +0200

    prvni verze

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Vítejte v mém novém e-shopu!</h1>
+      <p>Tohle je čistý start od začátku.</p>
+    </div>
+  );
+}
